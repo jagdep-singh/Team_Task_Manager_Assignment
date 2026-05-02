4a7952830ace985391b410e1a0a7935caaedfb5f
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-05-02 18:14:08 +0530

    db connection

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.db import Base, engine, SessionLocal
+from app.models.user import User
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def root():
     return {"message": "submission by Jagdeep Singh"}
 
+
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
 
+@app.post("/")
+def PingPong(msg : str):
+    if msg == "ping":
+        return {"message": "pong"}
+    else:
+        return {"message": "should have sent ping"}
+
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
