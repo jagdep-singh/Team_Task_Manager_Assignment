77305a9ea1dd525358474a8dbe9017d599da8812
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-05-03 06:39:03 +0530

    backend connected to deplyed frontend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://task-manager-jazz.vercel.app",
+        "https://*.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
