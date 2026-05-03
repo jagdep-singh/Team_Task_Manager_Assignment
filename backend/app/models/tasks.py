e7742e6b7b62ca3a2d7d69ed58df0c2f71fc1272
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-05-03 22:46:45 +0530

    db datetime error fixed

--- backend/app/models/tasks.py
+++ backend/app/models/tasks.py
@@ -1,16 +1,13 @@
-
-
-from sqlalchemy import Column, Integer, String, Enum as SQLAlchemyEnum
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, Enum as SQLAlchemyEnum, DateTime
 from app.db import Base
 from enum import Enum
-
 
 class Status(str, Enum):
     todo = "todo"
     in_progress = "in_progress"
     done = "done"
-    
+
+
 class Priority(str, Enum):
     low = "low"
     medium = "medium"
@@ -23,9 +20,11 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     project_id = Column(Integer, index=True)
     title = Column(String, index=True)
-    created_by = Column(Integer , index = True)
+    created_by = Column(Integer, index=True)
     description = Column(String)
+
     status = Column(SQLAlchemyEnum(Status), default=Status.todo)
     priority = Column(SQLAlchemyEnum(Priority), default=Priority.medium)
+
     assigned_to = Column(Integer, nullable=True, index=True)
-    due_date = Column(datetime, nullable=True)
+    due_date = Column(DateTime, nullable=True)
